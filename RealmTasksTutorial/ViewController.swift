d05b7d92c1690105f55988be7430c89675684350
ybhartia <yash@bhartia.in>
2017-02-15 16:15:36 -0800

    Initial Commit

---
+++ RealmTasksTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTasksTutorial
+//
+//  Created by yash bhartia on 2/15/17.
+//  Copyright © 2017 YashBhartia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
