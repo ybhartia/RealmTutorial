ee83a96a78f2ceda1ce3584b02e944b8fd33e634
ybhartia <yash@bhartia.in>
2017-02-18 14:19:36 -0800

    Test

--- RealmTasksTutorial/ViewController.swift
+++ RealmTasksTutorial/ViewController.swift
@@ -1,25 +1,116 @@
-//
-//  ViewController.swift
-//  RealmTasksTutorial
-//
-//  Created by yash bhartia on 2/15/17.
-//  Copyright © 2017 YashBhartia. All rights reserved.
-//
-
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+// MARK: Model
+
+final class TaskList: Object {
+    dynamic var text = ""
+    dynamic var id = ""
+    let items = List<Task>()
+    
+    override static func primaryKey() -> String? {
+        return "id"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+final class Task: Object {
+    dynamic var text = ""
+    dynamic var completed = false
+}
+class ViewController: UITableViewController
+{
+    var items = List<Task>()
+    var notificationToken: NotificationToken!
+    var realm: Realm!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setupUI()
+        items.append(Task(value: ["text": "My First Task"]))
+        setupRealm()
+    }
+    
+    func add()
+    {
+        let alertController = UIAlertController(title: "New Task", message: "Enter Task Name", preferredStyle: .alert)
+        var alertTextField: UITextField!
+        alertController.addTextField { textField in
+            alertTextField = textField
+            textField.placeholder = "Task Name"
+        }
+        
+        alertController.addAction(UIAlertAction(title: "Add", style: .default) { _ in
+            guard let text = alertTextField.text , !text.isEmpty else { return }
+    
+            //self.items.append(Task(value: ["text": text]))
+            //self.tableView.reloadData()
+            let items = self.items
+            try! items.realm?.write {
+                items.insert(Task(value: ["text": text]), at: items.filter("completed = false").count)
+            }
+        })
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func setupUI()
+    {
+        title = "My Tasks"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
+    }
+    
+    func setupRealm()
+    {
+        // Log in existing user with username and password
+        let username = "yash@bhartia.in"  // <--- Update this
+        let password = "qwerty123"  // <--- Update this
+        
+        SyncUser.logIn(with: .usernamePassword(username: username, password: password, register: false), server: URL(string: "http://127.0.0.1:9080")!) { user, error in
+            guard let user = user else {
+                fatalError(String(describing: error))
+            }
+            
+            DispatchQueue.main.async {
+                // Open Realm
+                let configuration = Realm.Configuration(
+                    syncConfiguration: SyncConfiguration(user: user, realmURL: URL(string: "realm://127.0.0.1:9080/~/realmtasks")!)
+                )
+                self.realm = try! Realm(configuration: configuration)
+                
+                // Show initial tasks
+                func updateList() {
+                    if self.items.realm == nil, let list = self.realm.objects(TaskList.self).first {
+                        self.items = list.items
+                    }
+                    self.tableView.reloadData()
+                }
+                updateList()
+                
+                // Notify us when Realm changes
+                self.notificationToken = self.realm.addNotificationBlock { _ in
+                    updateList()
+                }
+            }
+        }
+    }
+
+    deinit {
+        notificationToken.stop()
+    }
+
+    
+    override func tableView(_ tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.text
+        cell.textLabel?.alpha = item.completed ? 0.5 : 1
+        return cell
+    }
+}
